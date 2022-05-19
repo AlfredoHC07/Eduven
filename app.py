fa679370669b1df9469d274669d2016c96860c90
AlfredoHC07 <alfredo09hernandez@gmail.com>
2022-05-19 14:57:02 -0400

    [ADD] Heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 # from db import Session, engine
-from models.user import Usuario
+from src.models.user import Usuario
 import psycopg2
 # from flask_login import LoginManager, login_user, logout_user, login_required
 
