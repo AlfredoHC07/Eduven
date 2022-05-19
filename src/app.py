9b01b75bf21a042b5b195d461a6622b8d5e77968
AlfredoHC07 <alfredo09hernandez@gmail.com>
2022-05-19 14:24:02 -0400

    [ADD] Conexion a la base de datos.

--- src/app.py
+++ src/app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from db import Session, engine
-from models.user import Usuario 
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+# from db import Session, engine
+from models.user import Usuario
+import psycopg2
 # from flask_login import LoginManager, login_user, logout_user, login_required
 
-from config import config
+# from config import config
 
-session = Session()
+# session = Session()
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
+
+db = psycopg2.connect(
+    user = 'zjusxcfmfwujtd',
+    password = 'bcaa13a0feeb9138b0d8c5a0d0c366bb59e2463b764b9ca246d339b839f288ea',
+    host = "ec2-3-228-235-79.compute-1.amazonaws.com", 
+    port = '5432',
+    database = 'd102ukg1pnng0u')
 
 # login_manager_app = LoginManager(app)
 
@@ -25,7 +34,7 @@ def login():
         user_post     = request.form['username']
         password_post = request.form['password']
         
-        user_logged = Usuario.user_logg(user_post)
+        user_logged = Usuario.user_logg(db, user_post)
         
         if user_logged != None:
             if user_post == user_logged[0] and password_post == '':
@@ -35,7 +44,6 @@ def login():
                 flash('Contraseña Incorrecta.')
                 return redirect(url_for('login'))
             elif user_post == user_logged[0] and password_post == user_logged[1]:
-                # login_user(user_logged)
                 return render_template('home/home.html')
             else:
                 flash('Ocurrio un error.')
@@ -60,27 +68,27 @@ def register():
         check_password = request.form['conf_password']
         full_name      = request.form['full_name']
         
-        user_checking = Usuario.user_logg(user)
+        user_checking = Usuario.user_logg(db, user)
         
         if user_checking:
             flash('El usuario ya existe en el Sistema.')
             return redirect(url_for('register'))
         elif user_checking == None:
-            if password != check_password:
+            if user == '' or full_name == '' or password == '' or check_password == '':
+                flash('Debe ingresas todos los datos')
+                return redirect(url_for('register'))
+            elif password != check_password:
                 flash('Las contraseñas no coinciden.')
                 return redirect(url_for('register'))
             else:
-                with engine.connect() as con:
-                    new_user = Usuario(
-                        user_name=user, 
-                        password=password, 
-                        full_name=full_name
-                    )
-                    session.add(new_user)
-                    session.commit()
-                    
+                user_res = (user, password, full_name)
+                try:
+                    Usuario.user_res(db, user_res)
                     flash('Usuario registrado con éxito.')
                     return redirect(url_for('login'))
+                except Exception as ex:
+                    print(ex)
+                    return redirect(url_for('login'))              
         else:
             flash('Ocurrio un error.')
             return redirect(url_for('register'))      
@@ -92,5 +100,5 @@ def home():
     return render_template('home/home.html')
     
 if __name__=='__main__':
-    app.config.from_object(config['development'])
-    app.run(port=3333)
+    # app.config.from_object(config['development'])
+    app.run()
--- src/models/user.py
+++ src/models/user.py
@@ -1,22 +1,29 @@
-from sqlalchemy import Column,String,Integer
-from db import Base, engine
+# from sqlalchemy import Column,String,Integer
+# from db import Base, engine
 import psycopg2
 
-class Usuario(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    user_name = Column(String(20),unique=True)
-    password = Column(String(16))
-    full_name = Column(String(70))
-    
-    def user_logg(user_post):
+class Usuario:
+    # __tablename__ = 'users'
+    # id = Column(Integer, autoincrement=True, primary_key=True)
+    # user_name = Column(String(20),unique=True)
+    # password = Column(String(16))
+    # full_name = Column(String(70))
+    def user_res(db, user):
+        cursor = db.cursor()
+        consulta = "users(user_name, password, full_name)"
         
-        conn = psycopg2.connect(user = 'postgres',
-                        host = "localhost", 
-                        port = '5432',
-                        database = 'eduven')
-
-        cursor = conn.cursor()  
+        try:
+            sql = f'INSERT INTO {consulta} VALUES{user};'
+            
+            cursor.execute(sql)
+            
+            db.commit()
+                
+        except Exception as ex:
+            print(ex)
+    
+    def user_logg(db, user_post):
+        cursor = db.cursor()  
         
         try:
             sql = """ SELECT id, user_name, password, full_name FROM users
@@ -35,24 +42,20 @@ class Usuario(Base):
         except Exception as ex:
             print(ex)
             
-    def get_by_id(id):
-        conn = psycopg2.connect(user = 'postgres',
-                        host = "localhost", 
-                        port = '5432',
-                        database = 'eduven')
-        try:
-            cursor = conn.cursor() 
-            sql = "SELECT id, username, fullname FROM user WHERE id = {}".format(id)
-            cursor.execute(sql)
-            row = cursor.fetchone()
-            if row != None:
-                return User(row[0], row[1], None, row[2])
-            else:
-                return None
-        except Exception as ex:
-            raise Exception(ex)
+    # def get_by_id(db, id):
+    #     try:
+    #         cursor = db.cursor() 
+    #         sql = "SELECT id, username, fullname FROM user WHERE id = {}".format(id)
+    #         cursor.execute(sql)
+    #         row = cursor.fetchone()
+    #         if row != None:
+    #             return User(row[0], row[1], None, row[2])
+    #         else:
+    #             return None
+    #     except Exception as ex:
+    #         raise Exception(ex)
     
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
     
     
     
